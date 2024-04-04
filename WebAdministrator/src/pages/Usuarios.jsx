eb5272ec18092d77a77a6354d30ec76661a5e8e9
DiegoSalm522 <A01736106@tec.mx>
2024-04-04 13:13:59 -0600

    Added Navigation Bar

---
+++ WebAdministrator/src/pages/Usuarios.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Usuarios = () => {
+	return (
+		<div
+			style={{
+				display: "flex",
+				justifyContent: "centre",
+				alignItems: "centre",
+				height: "100vh",
+			}}
+		>
+			<h1>
+                Usuarios
+			</h1>
+		</div>
+	);
+};
+
+export default Usuarios;
