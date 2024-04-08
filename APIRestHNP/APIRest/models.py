6d3a4a302092a95d8c1cf75b5bfb88ff6c2c3c06
Josafat <josafatgarciasarmientos@gmail.com>
2024-04-08 17:43:07 -0600

    more endpoints

--- APIRestHNP/APIRest/models.py
+++ APIRestHNP/APIRest/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils.timezone import now
+from django.contrib.auth.hashers import make_password
 
 # Create your models here.
 
@@ -7,11 +8,16 @@ from django.utils.timezone import now
 class Usuario(models.Model):
     #insertar campos  #hashear contraseña
     user_id = models.AutoField(primary_key=True)
-    nombre = models.CharField(max_length=50)
+    nombre = models.CharField(max_length=50, unique=True)
     contrasena = models.CharField(max_length=50)
-    correo = models.CharField(max_length=50)
+    correo = models.CharField(max_length=50, unique=True)
     ventas_semana = models.IntegerField(default=0)
     supermodel = models.BooleanField(default=False)
+    active = models.BooleanField(default=True)
+    
+    def save(self, *args, **kwargs):
+        self.contrasena = make_password(self.contrasena)
+        super().save(*args, **kwargs)
     
     def as_dict(self):
         return {
@@ -20,16 +26,18 @@ class Usuario(models.Model):
             "contraseña": self.contrasena,
             "correo": self.correo,
             "ventas_semana": self.ventas_semana,
-            "supermodel": self.supermodel
+            "supermodel": self.supermodel,
+            "active": self.active
         }
         
 class Venta(models.Model):
     #insertar campos
     venta_id = models.AutoField(primary_key=True)
     user_id = models.ForeignKey(Usuario, editable=False, on_delete=models.DO_NOTHING) ##
-    monto = models.IntegerField(default=0)
+    monto = models.FloatField(default=0)
     cantidad = models.IntegerField(default=0)
     fecha = models.DateField(default=now, editable=False)
+    active = models.BooleanField(default=True)
     
     def as_dict(self):
         return { 
@@ -37,6 +45,7 @@ class Venta(models.Model):
                 "user_id": self.user_id.user_id,
                 "cantidad": self.cantidad,
                 "fecha": self.fecha,
-                "venta_id": self.venta_id
+                "venta_id": self.venta_id,
+                "active": self.active
                 }
 
--- APIRestHNP/APIRest/urls.py
+++ APIRestHNP/APIRest/urls.py
@@ -3,6 +3,10 @@ from . import views
 
 urlpatterns = [
     path("registrousuario/", views.registrousuario, name="registrousuario"),
+    path("usuarios/", views.usuarios, name="usuarios"),
     path("registroventa/", views.registroventa, name="registroventa"),
-    path("ventas/", views.ventas, name="ventas")
+    path("ventas/", views.ventas, name="ventas"),
+    path("eliminarusuario/<int:user_id>", views.eliminarusuario, name="eliminarusuario"),
+    path("eliminarventa/<int:venta_id>", views.eliminarventa, name="eliminarventa"),
+    path("ventasusuario/<int:user_id>", views.ventasusuario, name="ventasusuario")
 ]
