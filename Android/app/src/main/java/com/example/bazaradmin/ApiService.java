65958ac3a7ae994dfe2c766ba6bb86acb525c376
ErwinPo <A01734881@tec.mx>
2024-04-10 13:01:13 -0600

    added api endpoints to android (unfinished)

---
+++ Android/app/src/main/java/com/example/bazaradmin/ApiService.java
@@ -0,0 +1,12 @@
+package com.example.bazaradmin;
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.Path;
+
+public interface ApiService {
+    @GET("/quote")
+    Call<Quote> getPost();
+
+}
