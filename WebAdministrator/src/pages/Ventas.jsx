1a4f6ca27793aee8b09e407970bd6bfdff213425
DanAcoVaz <a01736279@tec.mx>
2024-04-16 19:28:55 -0600

    Se me esta petando todo, este es un backup. Se agrego de manera muy basica los inputs y el boton de submit con bootstrap

--- WebAdministrator/src/pages/Ventas.jsx
+++ WebAdministrator/src/pages/Ventas.jsx
@@ -1,19 +1,27 @@
 import React from "react";
 import Navbar from "../components/NavBar/Navbar";
+import classes from "./Ventas.module.css";
+import { Button, Form } from "react-bootstrap";
 
 const Ventas = () => {
 	return (
-		<div
-			style={{
-				display: "flex",
-				justifyContent: "centre",
-				flexDirection: "column",
-				alignItems: "centre",
-				height: "100vh",
-			}}
-		>
+		<div>
 			<Navbar />
-			<h1>Ventas</h1>
+			<Form>
+				<Form.Group controlId="formMonto">
+					<Form.Label>Monto Total</Form.Label>
+					<Form.Control type="number"/>
+				</Form.Group>
+
+				<Form.Group controlId="formCantidad">
+					<Form.Label>Número Total de Articulos Vendidos</Form.Label>
+					<Form.Control type="number"/>
+				</Form.Group>
+
+				<Button variant="warning" type="submit" className={classes.register_btn}>
+					Registrar Venta
+				</Button>
+			</Form>
 		</div>
 	);
 };
