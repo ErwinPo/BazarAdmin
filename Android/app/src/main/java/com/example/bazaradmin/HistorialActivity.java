a642ff8df6fa5a4f470f7827cae2e5e4043e2dc7
ErwinPo <A01734881@tec.mx>
2024-04-25 17:50:49 -0600

    completed sale history

---
+++ Android/app/src/main/java/com/example/bazaradmin/HistorialActivity.java
@@ -0,0 +1,86 @@
+package com.example.bazaradmin;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.tabs.TabLayout;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class HistorialActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    Ventas salesList;
+    SalesAdapter myAdapter;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_historial);
+
+
+        myAdapter = new SalesAdapter(HistorialActivity.this, salesList);
+
+        recyclerView = findViewById(R.id.historialRecyclerView);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+
+        SalesChangeListener();
+
+    }
+
+    Retrofit retrofit = new Retrofit.Builder()
+            .baseUrl("http://18.222.68.166:8000/BAZARAPI/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build();
+
+    private void SalesChangeListener() {
+        ApiService service = retrofit.create(ApiService.class);
+        Call<Ventas> call = service.getVentas();
+        Log.i("2","2");
+        call.clone().enqueue(new Callback<Ventas>() {
+            @Override
+            public void onResponse(Call<Ventas> call, Response<Ventas> response) {
+                String responseString = "RCode: " + response.code();
+                Log.i("RCODE", responseString);
+                if (response.isSuccessful()) {
+
+                    Ventas ventas = response.body();
+
+                    myAdapter = new SalesAdapter(getApplicationContext(), ventas);
+                    recyclerView.setAdapter(myAdapter);
+                    Log.i("ID OF LAST SALE", String.valueOf(ventas.registros.get(ventas.registros.size()-1).sale_id));
+
+                }
+            }
+            @Override
+            public void onFailure(Call<Ventas> call, Throwable t) {
+                Log.i("QUOTE", t.toString());
+                call.cancel();
+            };
+        });
+    }
+
+
+}
---
+++ Android/app/src/main/java/com/example/bazaradmin/SalesAdapter.java
@@ -0,0 +1,66 @@
+package com.example.bazaradmin;
+import android.content.Context;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Locale;
+
+
+public class SalesAdapter extends RecyclerView.Adapter<SalesAdapter.MyViewHolder> {
+
+    Context context;
+    Ventas SalesList;
+
+
+    public SalesAdapter(Context context, Ventas SalesArrayList) {
+        this.context = context;
+        this.SalesList = SalesArrayList;
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        View v = LayoutInflater.from(context).inflate(R.layout.registroitem, parent, false);
+
+        return new MyViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+        GetVenta sale = SalesList.registros.get(position);
+        holder.amount.setText("$"+String.valueOf(sale.amount));
+
+        SimpleDateFormat dateFormat = new SimpleDateFormat("d/M - hh:mm", new Locale("es","ES"));
+        holder.fecha.setText(dateFormat.format(sale.date));
+        holder.quantity.setText(String.valueOf(sale.quantity));
+        holder.sale_id.setText("#"+String.valueOf(sale.sale_id));
+    }
+
+
+    @Override
+    public int getItemCount() {
+        return SalesList.registros.size();
+    }
+
+    public  static class MyViewHolder extends RecyclerView.ViewHolder{
+        TextView sale_id,fecha,quantity, amount;
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            sale_id = itemView.findViewById(R.id.ventaid);
+            fecha = itemView.findViewById(R.id.fecha);
+            quantity = itemView.findViewById(R.id.cantidad);
+            amount = itemView.findViewById(R.id.amount);
+        }
+    }
+}
