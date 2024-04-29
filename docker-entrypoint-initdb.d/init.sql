a28d82cdac407210336866e915eb3eb3122fc1ad
Josafat <josafatgarciasarmientos@gmail.com>
2024-04-29 13:15:49 -0600

    fixed table names in call proc

--- docker-entrypoint-initdb.d/init.sql
+++ docker-entrypoint-initdb.d/init.sql
@@ -11,7 +11,7 @@ DROP PROCEDURE IF EXISTS SalesDateRangeSellerAmount$$
 CREATE PROCEDURE SalesDateRangeSellerAmount(IN start_date DATE, IN end_date DATE, IN user_id INT)
 BEGIN
 	SELECT SUM(amount) as day_amount, DATE(date) AS day
-	FROM apirest_sale
+	FROM APIRest_sale
 	WHERE date BETWEEN start_date AND end_date
 	AND user_id_id = user_id
 	GROUP BY DATE(date);
@@ -28,7 +28,7 @@ DROP PROCEDURE IF EXISTS SalesDateRangeSellerQuantity$$
 CREATE PROCEDURE SalesDateRangeSellerQuantity(IN start_date DATE, IN end_date DATE, IN user_id INT)
 BEGIN
 	SELECT SUM(quantity) as day_quantity, DATE(date) AS day
-	FROM apirest_sale
+	FROM APIRest_sale
 	WHERE date BETWEEN start_date AND end_date
 	AND user_id_id = user_id
 	GROUP BY DATE(date);
@@ -45,7 +45,7 @@ DROP PROCEDURE IF EXISTS SalesDateRangeAmount$$
 CREATE PROCEDURE SalesDateRangeAmount(IN start_date DATE, IN end_date DATE)
 BEGIN
 	SELECT SUM(amount) as day_amount, DATE(date) AS day
-	FROM apirest_sale
+	FROM APIRest_sale
 	WHERE date BETWEEN start_date AND end_date
 	GROUP BY DATE(date);
 END$$
@@ -61,7 +61,7 @@ DROP PROCEDURE IF EXISTS SalesDateRangeQuantity$$
 CREATE PROCEDURE SalesDateRangeQuantity(IN start_date DATE, IN end_date DATE)
 BEGIN
 	SELECT SUM(quantity) as day_quantity, DATE(date) AS day
-	FROM apirest_sale
+	FROM APIRest_sale
 	WHERE date BETWEEN start_date AND end_date
 	GROUP BY DATE(date);
 END$$
@@ -80,7 +80,7 @@ CREATE PROCEDURE Ranking()
 BEGIN
 
 	SELECT user_id_id AS user, SUM(amount) as amount 
-	FROM apirest_sale 
+	FROM APIRest_sale 
 	WHERE date >= DATE_SUB(CURDATE(), INTERVAL 7 DAY) 
 	GROUP BY user_id_id; 
 
