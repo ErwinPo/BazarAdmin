bbda2d8514c74651ee07f6abbc179c3580baa7e7
David Mtz <mtzmolinad35@gmail.com>
2024-05-02 22:04:11 -0700

    changes in sales graph

--- WebAdministrator/src/components/Statistics/SalesGraph.jsx
+++ WebAdministrator/src/components/Statistics/SalesGraph.jsx
@@ -4,42 +4,42 @@ import { SparkLineChart } from '@mui/x-charts/SparkLineChart';
 import { Stack, Box } from '@mui/material';
 
 const SalesGraph = ({salesData}) => {
-    // console.log("Sales Graph Sales: ", ...salesData)
+    if (!salesData || salesData.length === 0) {
+        return (
+            <Row className={classes.row}>
+                <Col md="10" className={classes.col}>
+                    <Card className={classes.card}>
+                        <h4 className={classes.text}> Estadísticas - Ganancias a través del tiempo </h4>
+                        <div>
+                            <h3>No available data for now</h3>
+                        </div>
+                    </Card>
+                </Col>
+            </Row>
+        );
+    }
 
-    const intervalTimes = salesData?.map(item => new Date(item.interval_time)); // Assuming interval_time is in ISO format
-    const totalAmounts = salesData?.map(item => item.total_amount);
-
-    // console.log("Interval Times: ",intervalTimes)
-    // console.log("totalAmounts: ",totalAmounts)
+    const intervalTimes = salesData.map(item => new Date(item.interval_time)); // Assuming interval_time is in ISO format
+    const totalAmounts = salesData.map(item => item.total_amount);
 
     return (
         <Row className={classes.row}>
             <Col md="10" className={classes.col}>
                 <Card className={classes.card}>
                     <h4 className={classes.text}> Estadísticas - Ganancias a través del tiempo </h4>
-
                     <Stack direction="row" sx={{ width: '75%' }} className={classes.stack}>
-                        {
-                            salesData ? (
-                                <SparkLineChart
-                                    data={totalAmounts}
-                                    xAxis={{
-                                        scaleType: 'time',
-                                        data: intervalTimes,
-                                        valueFormatter: (value) => value.toISOString().slice(0, 10),
-
-                                    }}
-                                    colors={['#222222']}
-                                    height={150}
-                                    showTooltip
-                                    showHighlight
-                                />
-                            ) : (
-                                <div>
-                                    <h3>No available data for now</h3>
-                                </div>
-                            )
-                        }
+                        <SparkLineChart
+                            data={totalAmounts}
+                            xAxis={{
+                                scaleType: 'time',
+                                data: intervalTimes,
+                                valueFormatter: (value) => value.toISOString().slice(0, 10),
+                            }}
+                            colors={['#222222']}
+                            height={150}
+                            showTooltip
+                            showHighlight
+                        />
                     </Stack>
                 </Card>
             </Col>
