3e3a0855dee923db04dff210a9381259bc0f4648
David Mtz <mtzmolinad35@gmail.com>
2024-05-20 19:22:19 -0700

    dynamic temporaliries & personalized dates range

--- WebAdministrator/src/components/Statistics/DatesDropdown.jsx
+++ WebAdministrator/src/components/Statistics/DatesDropdown.jsx
@@ -6,12 +6,13 @@ import DateRangePicker from '../DateRangePicker';
 const DatesDropdown = ({ onSalesDataUpdate, onRangeOfDatesUpdate, onItemsDataUpdate, currentUserId, currentUserData }) => {
     const [daysFromHandleDropdownItem, setDaysFromHandleDropdownItem] = useState([]);
     const [selectedOption, setSelectedOption] = useState(null);
-    const [showModal, setShowModal] = useState(false); // Changed from 'show' to 'showModal'
-    const [temporality, setTemporality] = useState('')
+    const [showModal, setShowModal] = useState(false);
+    const [startDate, setStartDate] = useState(null);
+    const [endDate, setEndDate] = useState(null);
+    const [temporality, setTemporality] = useState(null);
 
-    const handleClose = () => setShowModal(false); // Changed from 'setShowModal' to 'setShow'
+    const handleClose = () => setShowModal(false);
     const handleShowModal = () => setShowModal(true);
-    
 
     const getEndDate = () => {
         return new Date().toJSON().slice(0, 10);
@@ -19,68 +20,78 @@ const DatesDropdown = ({ onSalesDataUpdate, onRangeOfDatesUpdate, onItemsDataUpd
 
     const getCurrentDate = (startDate, days) => {
         const endDate = new Date(startDate);
-        endDate.setDate(endDate.getDate() - days); // Subtracting days directly from the date
+        endDate.setDate(endDate.getDate() - days);
         return endDate.toJSON().slice(0, 10);
     };
 
-    const calculateDifferenceInDays = () => {
-        const [startDate, endDate] = daysFromHandleDropdownItem;
-        if (!startDate || !endDate) return;
+    const calculateDifferenceInDays = (startDate, endDate) => {
+        if (!startDate || !endDate) return 0;
 
         const start = new Date(startDate);
         const end = new Date(endDate);
         const differenceInTime = end.getTime() - start.getTime();
-        const differenceInDays = differenceInTime / (1000 * 3600 * 24);
-        return differenceInDays;
+        return differenceInTime / (1000 * 3600 * 24);
     };
 
-    const handleTemporality = () => {
-        differenceInDays = calculateDifferenceInDays()
-
-        if (differenceInDays > 0 && differenceInDays <= 27){
-            setTemporality('daily')
+    const determineTemporality = (differenceInDays) => {
+        if (differenceInDays > 365) {
+            return 'annually';
+        } else if (differenceInDays > 32 && differenceInDays <= 365) {
+            return 'monthly';
+        } else if (differenceInDays > 7 && differenceInDays <= 32) {
+            return 'weekly';
+        } else {
+            return 'daily';
         }
-
-    }
-
-    console.log("Difference in days: ",calculateDifferenceInDays())
+    };
 
     const handleDropdownItemClick = (days) => {
         const endDate = getEndDate();
         const currDate = getCurrentDate(endDate, days);
 
+        const differenceInDays = calculateDifferenceInDays(currDate, endDate);
+        const temp = determineTemporality(differenceInDays);
+
         setDaysFromHandleDropdownItem([currDate, endDate]);
+        setTemporality(temp);
         onRangeOfDatesUpdate([currDate, endDate]);
-        setSelectedOption(days); // Update selected option
+        setSelectedOption(days);
+
+        console.log("temporality: ", temp);
+        console.log("endDate: ", endDate);
+    };
+
+    const handleApplyCustomDates = () => {
+        if (startDate && endDate) {
+            const formattedStartDate = startDate.toJSON().slice(0, 10);
+            const formattedEndDate = endDate.toJSON().slice(0, 10);
+
+            const differenceInDays = calculateDifferenceInDays(formattedStartDate, formattedEndDate);
+            const temp = determineTemporality(differenceInDays);
+
+            setDaysFromHandleDropdownItem([formattedStartDate, formattedEndDate]);
+            setTemporality(temp);
+            onRangeOfDatesUpdate([formattedStartDate, formattedEndDate]);
+            setShowModal(false);
+            setSelectedOption(null);
+
+            console.log("temporality: ", temp);
+        }
     };
 
     useEffect(() => {
         const [startDate, endDate] = daysFromHandleDropdownItem;
+        if (!startDate || !endDate || !temporality) return;
 
-        // console.log("Current user id dates dropdown: ",currentUserId)
+        let itemsFetch = `http://3.146.65.111:8000/bazar/sales-date-range-quantity/?start-date=${startDate}&end-date=${endDate}&temporality=${temporality}`;
+        let salesFetch = `http://3.146.65.111:8000/bazar/sales-date-range-amount/?start-date=${startDate}&end-date=${endDate}&temporality=${temporality}`;
 
-        console.log("Current user data", currentUserData)
-        // console.log("Current user data", currentUserData.id)
-        // console.log("Current user data", currentUserId)
-        // console.log("Current user id", currentUserData.id)
-        // console.log("Current user id", currentUserData.id)
+        if (currentUserData) {
+            itemsFetch = `http://3.146.65.111:8000/bazar/sales-date-range-quantity-seller/?start-date=${startDate}&end-date=${endDate}&temporality=${temporality}&id=${currentUserData.id}`;
+            salesFetch = `http://3.146.65.111:8000/bazar/sales-date-range-amount-seller/?start-date=${startDate}&end-date=${endDate}&temporality=${temporality}&id=${currentUserData.id}`;
+        }
 
-    
-        if (startDate && endDate) {
-            // Fetch to get total items
-
-            let itemsFetch = `http://3.146.65.111:8000/bazar/sales-date-range-quantity/?start-date=${startDate}&end-date=${endDate}&temporality=daily`
-            let salesFetch =  `http://3.146.65.111:8000/bazar/sales-date-range-amount/?start-date=${startDate}&end-date=${endDate}&temporality=daily`
-
-
-            if(currentUserData){
-                itemsFetch = `http://3.146.65.111:8000/bazar/sales-date-range-quantity-seller/?start-date=${startDate}&end-date=${endDate}&temporality=daily&id=${currentUserData.id}`
-                salesFetch += `http://3.146.65.111:8000/bazar/sales-date-range-amount-seller/?start-date=${startDate}&end-date=${endDate}&temporality=daily&id=${currentUserData.id}`
-            }
-
-            fetch(itemsFetch, {
-                method: "GET"
-            })
+        fetch(itemsFetch, { method: "GET" })
             .then(response => {
                 if (!response.ok) {
                     throw new Error('Error al cargar los datos del servidor');
@@ -88,17 +99,13 @@ const DatesDropdown = ({ onSalesDataUpdate, onRangeOfDatesUpdate, onItemsDataUpd
                 return response.json();
             })
             .then(data => {
-                onItemsDataUpdate(data)
-                // console.log("Items data:", data)
+                onItemsDataUpdate(data);
             })
             .catch(error => {
                 console.error('Error:', error);
             });
-    
-            // Fetch to get total sales amount
-            fetch(salesFetch, {
-                method: "GET"
-            })
+
+        fetch(salesFetch, { method: "GET" })
             .then(response => {
                 if (!response.ok) {
                     throw new Error('Error al cargar los datos del servidor');
@@ -111,53 +118,34 @@ const DatesDropdown = ({ onSalesDataUpdate, onRangeOfDatesUpdate, onItemsDataUpd
             .catch(error => {
                 console.error('Error:', error);
             });
-            console.log(...daysFromHandleDropdownItem)
-        }
-    }, [daysFromHandleDropdownItem, currentUserData]);
-    
+
+        console.log("Rango de fechas: ", daysFromHandleDropdownItem);
+        console.log("Temporalidad: ", temporality);
+    }, [daysFromHandleDropdownItem, temporality, currentUserData]);
 
     return (
         <>
             <Dropdown>
                 <Dropdown.Toggle variant="success" id="dropdown-basic">
-                    {daysFromHandleDropdownItem.length > 0  ? `${daysFromHandleDropdownItem[0]} - ${daysFromHandleDropdownItem[1]}` : 'Rango de Fechas'}
-                    
+                    {daysFromHandleDropdownItem.length > 0 ? `${daysFromHandleDropdownItem[0]} - ${daysFromHandleDropdownItem[1]}` : 'Rango de Fechas'}
                 </Dropdown.Toggle>
                 <Dropdown.Menu>
-                    <Dropdown.Item
-                        onClick={() => handleDropdownItemClick(1)}
-                        className={selectedOption === 1 ? classes.selectedOption : ""}
-                    >
+                    <Dropdown.Item onClick={() => handleDropdownItemClick(1)} className={selectedOption === 1 ? classes.selectedOption : ""}>
                         Hace 1 día
                     </Dropdown.Item>
-                    <Dropdown.Item
-                        onClick={() => handleDropdownItemClick(7)}
-                        className={selectedOption === 7 ? classes.selectedOption : ""}
-                    >
+                    <Dropdown.Item onClick={() => handleDropdownItemClick(7)} className={selectedOption === 7 ? classes.selectedOption : ""}>
                         Hace 7 días
                     </Dropdown.Item>
-                    <Dropdown.Item
-                        onClick={() => handleDropdownItemClick(15)}
-                        className={selectedOption === 15 ? classes.selectedOption : ""}
-                    >
+                    <Dropdown.Item onClick={() => handleDropdownItemClick(15)} className={selectedOption === 15 ? classes.selectedOption : ""}>
                         Hace 15 días
                     </Dropdown.Item>
-                    <Dropdown.Item
-                        onClick={() => handleDropdownItemClick(30)}
-                        className={selectedOption === 30 ? classes.selectedOption : ""}
-                    >
+                    <Dropdown.Item onClick={() => handleDropdownItemClick(30)} className={selectedOption === 30 ? classes.selectedOption : ""}>
                         Hace 30 días
                     </Dropdown.Item>
-                    <Dropdown.Item
-                        onClick={() => handleDropdownItemClick(180)}
-                        className={selectedOption === 180 ? classes.selectedOption : ""}
-                    >
+                    <Dropdown.Item onClick={() => handleDropdownItemClick(180)} className={selectedOption === 180 ? classes.selectedOption : ""}>
                         Hace 6 meses
                     </Dropdown.Item>
-                    <Dropdown.Item
-                        onClick={handleShowModal}
-                        className={selectedOption === null ? classes.selectedOption : ""}
-                    >
+                    <Dropdown.Item onClick={handleShowModal} className={selectedOption === null ? classes.selectedOption : ""}>
                         Personalizar
                     </Dropdown.Item>
                 </Dropdown.Menu>
@@ -167,13 +155,18 @@ const DatesDropdown = ({ onSalesDataUpdate, onRangeOfDatesUpdate, onItemsDataUpd
                     <Modal.Title>Rango de fechas: Personalizable</Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
-                    <DateRangePicker/>
+                    <DateRangePicker
+                        startDate={startDate}
+                        endDate={endDate}
+                        handleStartDateChange={date => setStartDate(date)}
+                        handleEndDateChange={date => setEndDate(date)}
+                    />
                 </Modal.Body>
                 <Modal.Footer>
                     <Button variant="secondary" onClick={handleClose}>
                         Cancelar
                     </Button>
-                    <Button variant="primary" onClick={handleClose}>
+                    <Button variant="primary" onClick={handleApplyCustomDates}>
                         Aplicar
                     </Button>
                 </Modal.Footer>
--- WebAdministrator/src/components/Statistics/SalesGraph.jsx
+++ WebAdministrator/src/components/Statistics/SalesGraph.jsx
@@ -1,106 +1,85 @@
-import { Dropdown, Container, Card, Col, Row } from 'react-bootstrap';
-import classes from "./StatisticsSalesGraph.module.css";
-import { SparkLineChart } from '@mui/x-charts/SparkLineChart';
+import React, { useState, useEffect } from 'react';
 import { BarChart } from '@mui/x-charts/BarChart';
 import { Stack, Box, Button } from '@mui/material';
-import { axisClasses } from '@mui/x-charts/ChartsAxis';
-import { useState, useEffect } from 'react';
+import { Row, Col, Card } from 'react-bootstrap';
+import classes from "./StatisticsSalesGraph.module.css";
 
 const SalesGraph = ({ salesData, itemsData }) => {
-    const [isSales, setIsSales] = useState(true);
-    const [currentData, setCurrentData] = useState({ dates: [], amounts: [] });
+  const [isSales, setIsSales] = useState(true);
+  const [currentData, setCurrentData] = useState({ dates: [], amounts: [] });
 
-    useEffect(() => {
-        updateData();
-    }, [isSales, salesData, itemsData]);
+  useEffect(() => {
+    updateData();
+  }, [isSales, salesData, itemsData]);
 
-    const updateData = () => {
-        const dataArray = isSales ? salesData?.qty : itemsData?.qty;
+  const updateData = () => {
+    const dataArray = isSales ? salesData?.qty : itemsData?.qty;
 
-        if (!dataArray) {
-            setCurrentData({ dates: [], amounts: [] });
-            return;
-        }
+    if (!dataArray) {
+      setCurrentData({ dates: [], amounts: [] });
+      return;
+    }
 
-        const dates = dataArray.map(item => new Date(item.interval_time));
-        const amounts = dataArray.map(item => isSales ? item.total_amount : item.total_quantity);
-        dates.sort((a, b) => a - b);
-        setCurrentData({ dates, amounts });
-    };
+    const dates = dataArray.map(item => new Date(item.interval_time));
+    const amounts = dataArray.map(item => isSales ? item.total_amount : item.total_quantity);
+    dates.sort((a, b) => a - b);
+    setCurrentData({ dates, amounts });
+  };
 
-    const handleToggle = () => {
-        setIsSales(!isSales);
-    };
+  const handleToggle = () => {
+    setIsSales(!isSales);
+  };
 
-    const valueFormatter = (value) => isSales ? `$${value.toFixed(2)}` : `${value} objetos`;
+  const valueFormatter = (value) => isSales ? `$${value.toFixed(2)}` : `${value} objetos`;
 
-    const data = currentData.dates.map((time, index) => ({
-        interval_time: time.toISOString().slice(0, 10),
-        total_amount: currentData.amounts[index],
-    }));
+  const data = currentData.dates.map((time, index) => ({
+    interval_time: time.toISOString().slice(0, 10),
+    total_amount: currentData.amounts[index],
+  }));
 
-    const chartSetting = {
-        yAxis: [
-            {
-                label: isSales ? 'Ventas ($)' : 'Objetos vendidos',
-            },
-        ],
-        series: [{ dataKey: 'total_amount', label: isSales ? 'Ventas del Bazar' : 'Objetos del Bazar', valueFormatter: valueFormatter }],
-        height: 300,
-        sx: {
-            [`& .${axisClasses.directionY} .${axisClasses.label}`]: {
-                transform: 'translateX(-10px)',
-            },
-        },
-    };
+  const chartSetting = {
+    yAxis: [
+      {
+        label: isSales ? 'Ventas ($)' : 'Objetos vendidos',
+      },
+    ],
+    series: [{ dataKey: 'total_amount', label: isSales ? 'Ventas del Bazar' : 'Objetos del Bazar', valueFormatter: valueFormatter }],
+    height: 300,
+    sx: {
+      [`& .MuiChartsAxis-axisY .MuiChartsAxis-label`]: {
+        transform: 'translateX(-10px)',
+      },
+    },
+  };
 
-    return (
-        <>
-            <Row className={classes.row}>
-                <Col md="10" className={classes.col}>
-                    <Card className={classes.card}>
-                        <h4 className={classes.text}> Estadísticas - Ganancias a través del tiempo </h4>
-                        <Stack direction="row" sx={{ width: '90%' }} className={classes.stack}>
-                            <BarChart
-                                dataset={data}
-                                xAxis={[{ scaleType: 'band', dataKey: 'interval_time' }]}
-                                grid={{ horizontal: true }}
-                                {...chartSetting}
-                                height={400}
-                            />
-                        </Stack>
-                        <Box className="d-flex justify-content-end">
-                            <Button variant="contained" color="primary" style={{ textTransform: 'none' }} onClick={handleToggle}>
-                                {isSales ? 'Ventas' : 'Objetos'}
-                            </Button>
-                        </Box>
-                    </Card>
-                </Col>
-            </Row>
-
-            <Row className={classes.row}>
-                <Col md="10" className={classes.col}>
-                    <Card className={classes.card}>
-                        <h4 className={classes.text}> Estadísticas - Ganancias a través del tiempo </h4>
-                        <Stack direction="row" sx={{ width: '80%' }} className={classes.stack}>
-                            <SparkLineChart
-                                data={currentData.amounts}
-                                xAxis={{
-                                    scaleType: 'time',
-                                    data: currentData.dates,
-                                    valueFormatter: (value) => value.toISOString().slice(0, 10),
-                                }}
-                                colors={['#222222']}
-                                height={150}
-                                showTooltip
-                                showHighlight
-                            />
-                        </Stack>
-                    </Card>
-                </Col>
-            </Row>
-        </>
-    );
+  return (
+    <>
+      <Row className={classes.row}>
+        <Col md="10" className={classes.col}>
+          <Card className={classes.card}>
+            <h4 className={classes.text}> Estadísticas - Ganancias a través del tiempo </h4>
+            <Stack direction="row" sx={{ width: '90%' }} className={classes.stack}>
+              <BarChart
+                slotProps={{
+                  noDataOverlay: { message: 'No data to display in this chart' },
+                }}
+                dataset={data.length > 0 ? data : []}
+                xAxis={[{ scaleType: 'band', dataKey: 'interval_time' }]}
+                grid={{ horizontal: true }}
+                {...chartSetting}
+                height={400}
+              />
+            </Stack>
+            <Box className="d-flex justify-content-end">
+              <Button variant="contained" color="primary" style={{ textTransform: 'none' }} onClick={handleToggle}>
+                {isSales ? 'Ventas' : 'Objetos'}
+              </Button>
+            </Box>
+          </Card>
+        </Col>
+      </Row>
+    </>
+  );
 };
 
 export default SalesGraph;
