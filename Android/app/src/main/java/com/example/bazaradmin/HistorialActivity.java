4faeacb3461315319eae6a653b515a2135bf534e
ErwinPo <A01734881@tec.mx>
2024-05-24 18:01:47 -0600

    Fixed incorrect import

--- Android/app/src/main/java/com/example/bazaradmin/HistorialActivity.java
+++ Android/app/src/main/java/com/example/bazaradmin/HistorialActivity.java
@@ -25,7 +25,6 @@ import java.util.Collection;
 import java.util.List;
 import java.util.Objects;
 
-import kotlin.coroutines.jvm.internal.SuspendFunction;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
