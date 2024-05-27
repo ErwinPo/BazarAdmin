7799371c96c320c0389c7ddb58774b2e12efda44
Josafat <josafatgarciasarmientos@gmail.com>
2024-05-26 22:54:00 -0600

    added .conf file for nginx

--- WebAdministrator/src/App.jsx
+++ WebAdministrator/src/App.jsx
@@ -1,11 +1,11 @@
 import React, { useState, useEffect, createContext } from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route,Navigate } from 'react-router-dom';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import Ventas from './pages/Ventas';
 import UsersView from './components/Users/UsersView';
 import Estadisticas from './pages/Estadisticas';
-import RecordsView from './components/Records/RecordsView';
+import RecordsView from "./components/Records/RecordsView";
 import Login from './components/Login/Login';
 import Download from './pages/Download';
 import ResetPassword from './pages/ResetPassword';
@@ -24,7 +24,7 @@ function App() {
             if (access_token && login_time) {
                 const now = new Date().getTime();
                 const timeElapsed = now - parseInt(login_time);
-                const time_ms = 4 * 60 * 60 * 1000; // 4 horas en milisegundos
+                const time_ms = 4 * 60 * 60 * 1000;
                 if (timeElapsed > time_ms) {
                     handleLogout("Su sesión ha expirado");
                 } else {
@@ -58,10 +58,6 @@ function App() {
         }
     }, [expirationMessage]);
 
-    const PrivateRoute = ({ children }) => {
-        return isLoggedIn ? children : <Navigate to="/" />;
-    };
-
     return (
         <AuthContext.Provider value={{ isLoggedIn, handleLogin, handleLogout }}>
             <ToastContainer position="top-center" autoClose={3000} />
@@ -71,27 +67,18 @@ function App() {
                         <Route path="/" element={isLoggedIn ? <Navigate to="/Ventas" /> : <Login />} />
                         <Route path="/reset-password/:uidb64/:token" element={<ResetPassword />} />
                         <Route path="/Descarga" element={<Download />} />
-                        <Route path="/Ventas" element={
-                            <PrivateRoute>
-                                <Ventas />
-                            </PrivateRoute>
-                        } />
-                        <Route path="/Usuarios" element={
-                            <PrivateRoute>
-                                <UsersView />
-                            </PrivateRoute>
-                        } />
-                        <Route path="/Estadisticas" element={
-                            <PrivateRoute>
-                                <Estadisticas />
-                            </PrivateRoute>
-                        } />
-                        <Route path="/Registros" element={
-                            <PrivateRoute>
-                                <RecordsView />
-                            </PrivateRoute>
-                        } />
-                        <Route path="/*" element={<Navigate to={isLoggedIn ? "/Ventas" : "/"} />} />
+                        {isLoggedIn && (
+                            <>
+                                <Route path="/Ventas" element={<Ventas />} />
+                                <Route path="/Usuarios" element={<UsersView />} />
+                                <Route path="/Estadisticas" element={<Estadisticas />} />
+                                <Route path="/Registros" element={<RecordsView />} />
+                                <Route path="/*" element={<Navigate to="/Ventas" replace />} />
+                            </>
+                        )}
+                        {!isLoggedIn && (
+                            <Route path="/*" element={<Navigate to="/" />} />
+                        )}
                     </Routes>
                 </Router>
             )}
@@ -100,4 +87,3 @@ function App() {
 }
 
 export default App;
-
